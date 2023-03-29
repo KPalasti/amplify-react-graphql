12dc4d74395667563abef3a9139c00e433b7f45d
KPalasti <89475786+KPalasti@users.noreply.github.com>
2023-03-29 14:06:02 -0600

    Report Id System

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
+      <Heading level={1}>Report Id System</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
